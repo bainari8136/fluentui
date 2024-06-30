132587800f5e0b5b84419593304bd55f7ac4a3f8
bainari8136 <williammsangazila@gmail.com>
2024-06-30 15:26:12 +0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import { Button } from '@fluentui/react-components';
+import './App.css';
+
+function App() {
+  
+
+  return(
+    <div>
+      <Button appearance="primary">
+        Get started
+      </Button>
+    </div>
+  );
+}
+
+export default App
